2d3e017913e6bab707e07c9bc802feacfbf6bb8f
Xavier M <xavier.andre1232@gmail.com>
2025-10-30 15:01:39 -0400

    Project init

---
+++ ITCS5102-Rust-Project/target/debug/deps/ITCS5102_Rust_Project-6216c9ef5f487ec1.d
@@ -0,0 +1,5 @@
+/Users/xaviermolina/Documents/GitHub/ITCS5102-Rust-Project/ITCS5102-Rust-Project/target/debug/deps/ITCS5102_Rust_Project-6216c9ef5f487ec1.d: src/main.rs
+
+/Users/xaviermolina/Documents/GitHub/ITCS5102-Rust-Project/ITCS5102-Rust-Project/target/debug/deps/libITCS5102_Rust_Project-6216c9ef5f487ec1.rmeta: src/main.rs
+
+src/main.rs:
---
+++ ITCS5102-Rust-Project/target/debug/deps/ITCS5102_Rust_Project-f28305cdda5f434e.d
@@ -0,0 +1,5 @@
+/Users/xaviermolina/Documents/GitHub/ITCS5102-Rust-Project/ITCS5102-Rust-Project/target/debug/deps/ITCS5102_Rust_Project-f28305cdda5f434e.d: src/main.rs
+
+/Users/xaviermolina/Documents/GitHub/ITCS5102-Rust-Project/ITCS5102-Rust-Project/target/debug/deps/libITCS5102_Rust_Project-f28305cdda5f434e.rmeta: src/main.rs
+
+src/main.rs:
